4282e388289611e5df01e922ce4614a7f1511412
Dazannah <beattius@gmail.com>
2025-08-21 21:40:15 +0200

    added asset serve

--- backend/route.go
+++ backend/route.go
@@ -1,11 +1,14 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func registerRoutes() *gin.Engine {
 
 	router := gin.Default()
 	router.Static("/static", "../frontend/dist")
+	router.Static("/assets", "../frontend/dist/assets")
 	router.StaticFile("/cv.pdf", "./static/cv.pdf")
 
 	router.NoRoute(func(c *gin.Context) {
